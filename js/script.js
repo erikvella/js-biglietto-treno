c55b041dce94b9bdea7906f0f45fd9384581c270
erikvella <vella.erik1@gmail.com>
2023-09-07 14:01:00 +0100

    [add] const

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 1 salvare le due variabili età e km da percorrere
+// 2 calcolare il prezzo del biglietto in base ai dati forniti
+// 3 stamparlo in pagina
+
+// 1
+const outputTag = document.getElementById('output');
+const age = parseInt(prompt('Digita la tua età')) ;
+const km = parseInt(prompt('Digita il numero di km da percorrere')) ;
+
